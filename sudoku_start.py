88999a2b6760aa907b3ea14399ea7e7782f0edb3
jmoss80917 <jmoss80917@gmail.com>
2025-11-30 15:24:10 -0700

    Add sudoku_start.py

---
+++ sudoku_start.py
@@ -0,0 +1,12 @@
+# My first Sudoku trainer - built with Grok's help
+print("Welcome to John's Sudoku Trainer!")
+print("Grid coming soon...")
+
+# A tiny taste of a Sudoku grid (empty for now)
+grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # ... (we'll fill this out later)
+]
+
+print("Empty 9x9 grid ready. What's next?")# Write your code here :-)
